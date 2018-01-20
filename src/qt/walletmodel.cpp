c806741848d57c3d6ebcbf14954f8f70e4f4e0d0
modzer0 <modzer0@phazer.org>
2018-01-20 16:47:52 -0400

    Fixed balance not updating

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -105,7 +105,7 @@ void WalletModel::pollBalanceChanged()
         // Balance and number of transactions might have changed
         cachedNumBlocks = nBestHeight;
 
-        fForceCheckBalanceChanged = true;
+        checkBalanceChanged();
         if(transactionTableModel)
             transactionTableModel->updateConfirmations();
     }
